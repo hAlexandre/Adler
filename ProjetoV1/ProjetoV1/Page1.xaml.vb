0acd3b60aaf68fa169a93b862ddbe43c5b01501c
Alexandre <alexandreoliveiraa1@hotmail.com>
2014-10-17 02:47:02 -0300

    Banco de dados
    Criei a tela de login e liguei o banco de dados

--- ProjetoV1/ProjetoV1/Page1.xaml.vb
+++ ProjetoV1/ProjetoV1/Page1.xaml.vb
@@ -1,3 +1,28 @@
 ﻿Class Page1 
 
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles Button1.Click
+
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.Windows.Controls.TextChangedEventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    
+    Private Sub Page1_Loaded(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles MyBase.Loaded
+       
+        Dim ThisMoment As Date
+        ThisMoment = Now
+
+        TextBox3.Text = ThisMoment
+
+
+
+    End Sub
+
+   
+    Private Sub Page1_Loaded_1(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles MyBase.Loaded
+
+    End Sub
 End Class
