90e78146d03c4770da1353bd5ee81629935cf8a1
Gabriel-aa <gabriel-almeida-andrade@hotmail.com>
2014-11-10 20:53:34 -0200

    Validação de Consulta de Usuários

--- V3 (delphi)/Alterar__usuario.pas
+++ V3 (delphi)/Alterar__usuario.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, Menus, Grids, DBGrids, DB, ADODB, StdCtrls;
+  Dialogs, Menus, Grids, DBGrids, DB, ADODB, StdCtrls, jpeg, ExtCtrls,
+  Buttons;
 
 type
   TAlterarUsuario = class(TForm)
@@ -25,7 +26,6 @@ type
     ADOQuery1: TADOQuery;
     DataSource1: TDataSource;
     DBGrid1: TDBGrid;
-    Button2: TButton;
     Edit2: TEdit;
     Button3: TButton;
     Button4: TButton;
@@ -34,6 +34,15 @@ type
     Label2: TLabel;
     Edit3: TEdit;
     Button6: TButton;
+    Image1: TImage;
+    Label7: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Edit4: TEdit;
+    Label6: TLabel;
+    Edit5: TEdit;
+    SpeedButton1: TSpeedButton;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
@@ -41,6 +50,9 @@ type
     procedure Button5Click(Sender: TObject);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure Button6Click(Sender: TObject);
+    procedure DBGrid1CellClick(Column: TColumn);
+    procedure DBGrid1Exit(Sender: TObject);
+    procedure SpeedButton1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -66,22 +78,34 @@ begin
                  ADOQuery1.SQL.Clear; {Limpa o conteúdo da propriedade SQL}
                  ADOQuery1.SQL.Add('SELECT * from Usuariodesistema');
                  ADOQuery1.SQL.Add('where usuario like'+#39+Edit1.text+'%'+#39);
-                 ADOQuery1.Open ;
+                 ADOQuery1.Open;
 end;
 
 procedure TAlterarUsuario.Button2Click(Sender: TObject);
 begin
-Edit2.Visible:=True;
-Edit3.Visible:=True;
+
+    Edit2.Visible:=True;
+    Edit3.Visible:=True;
+    Edit4.Visible:=True;
+    Edit5.Visible:=True;
     Edit2.text := '';
     Edit3.text := '';
-    Button2.Visible:=False;
+    Edit4.text := '';
+    Edit5.text := '';
+
     DBGrid1.Visible:=False;
     Label1.Visible:=True;
     Label2.Visible:=True;
     Button3.Visible:=True;
     Button4.Visible:=True;
     Button1.Enabled:=False;
+    Button5.Visible:=False;
+    Label3.Visible:=True;
+    Label4.Visible:=True;
+    Label5.Visible:=True;
+    Label6.Visible:=True;
+    Label7.Visible:=False;
+
 
 end;
 
@@ -90,16 +114,32 @@ begin
 
 
 
+             if edit2.Text <> edit5.Text then
+              begin
+                ShowMessage('A nova senha não confere');
+                exit;
+              end;
+
   ADOQuery1.refresh;
   BDUsuarios.ADOTable1.Open;
   aux := DBGrid1.DataSource.DataSet.FieldValues['Codigo'];
   if BDUsuarios.ADOTable1.Locate('Codigo',aux,[])    then
     begin
 
-
         BDUsuarios.ADOTable1.Edit;
-        BDUsuarios.ADOTable1.FieldByName('senha').Value:=edit2.Text;
-        BDUsuarios.ADOTable1.FieldByName('endereco').Value:=edit3.Text;
+
+          if edit2.Text <> '' then
+          begin
+            BDUsuarios.ADOTable1.FieldByName('senha').Value:=edit2.Text;
+          end;
+
+
+
+
+        if edit3.Text <> '' then
+        begin
+          BDUsuarios.ADOTable1.FieldByName('endereco').Value:=edit3.Text;
+        end;
         BDUsuarios.ADOTable1.Post;
     end;
     ADOQuery1.Refresh;
@@ -109,7 +149,6 @@ begin
     Edit2.Visible:=False;
     Edit3.Visible:=False;
     Button3.Visible:=False;
-    Button2.Visible:=True;
     Button4.Visible:=False;
     DBGrid1.Refresh;
     Edit2.Text:='';
@@ -125,8 +164,24 @@ end;
 
 procedure TAlterarUsuario.Button4Click(Sender: TObject);
 begin
-Self.Destroy;
-TelaInicial.Show;
+    Edit2.Visible:=False;
+    Edit3.Visible:=False;
+    Edit4.Visible:=False;
+    Edit5.Visible:=False;
+
+    SpeedButton1.Visible := True;
+    DBGrid1.Visible:=True;
+    Label1.Visible:=False;
+    Label2.Visible:=False;
+    Button3.Visible:=False;
+    Button4.Visible:=False;
+    Button5.Visible:=True;
+    Button1.Enabled:=True;
+    Label7.Visible:=True;
+    Label6.Visible:=False;
+    Label5.Visible:=False;
+    Label3.Visible:=False;
+    Label4.Visible:=False;
 end;
 
 procedure TAlterarUsuario.Button5Click(Sender: TObject);
@@ -136,7 +191,6 @@ begin
        if BDUsuarios.ADOTable1.Locate('codigo',aux,[])    then
        begin
         DBGrid1.DataSource.DataSet.Delete;
-
 
        end;
 end;
@@ -156,6 +210,47 @@ begin
   Edit3.Text:='';
   TelaInicial.show;
   Self.Hide;
+end;
+
+procedure TAlterarUsuario.DBGrid1CellClick(Column: TColumn);
+begin
+
+
+    SpeedButton1.Enabled:=True;
+
+
+end;
+
+procedure TAlterarUsuario.DBGrid1Exit(Sender: TObject);
+begin
+
+    SpeedButton1.Enabled:=False;
+end;
+
+procedure TAlterarUsuario.SpeedButton1Click(Sender: TObject);
+begin
+    Edit2.Visible:=True;
+    Edit3.Visible:=True;
+    Edit4.Visible:=True;
+    Edit5.Visible:=True;
+    Edit2.text := '';
+    Edit3.text := '';
+    Edit4.text := '';
+    Edit5.text := '';
+
+    SpeedButton1.Visible := False;
+    DBGrid1.Visible:=False;
+    Label1.Visible:=True;
+    Label2.Visible:=True;
+    Button3.Visible:=True;
+    Button4.Visible:=True;
+    Button1.Enabled:=False;
+    Button5.Visible:=False;
+    Label3.Visible:=True;
+    Label4.Visible:=True;
+    Label5.Visible:=True;
+    Label6.Visible:=True;
+    Label7.Visible:=False;
 end;
 
 end.
